489cff9f24da6fc48ac42f5fd6725450772cbde7
DsPeacemaker <enotikpartizan@mail.ru>
2023-09-23 23:06:57 +0300

    Создать класс чужого

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 from bullet import Bullet
+from alien import Alien
 
 
 class AlienInvasion:
@@ -19,6 +20,9 @@ class AlienInvasion:
         self.bg_color = (230, 230, 230)
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
+        self.aliens = pygame.sprite.Group()
+
+        self._creat_fleet()
 
     def run_game(self):
         '''Запуск основного цикла игры'''
@@ -84,7 +88,14 @@ class AlienInvasion:
         self.ship.blitme()
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
+        self.aliens.draw(self.screen)
         pygame.display.flip()
+
+    def _creat_fleet(self):
+        '''Создание флота вторжения'''
+        # Создание пришельца
+        alien = Alien(self)
+        self.aliens.add(alien)
 
 
 if __name__ == '__main__':
