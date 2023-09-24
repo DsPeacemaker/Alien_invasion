b63b2e6c2afeb2d2f62f7e36e24981a7cd8abdc4
DsPeacemaker <enotikpartizan@mail.ru>
2023-09-24 15:53:28 +0300

    Добавить флот чужих на экран

--- alien_invasion.py
+++ alien_invasion.py
@@ -93,9 +93,20 @@ class AlienInvasion:
 
     def _creat_fleet(self):
         '''Создание флота вторжения'''
-        # Создание пришельца
+        # Создание пришельца и вычисление количества пришельцев в ряду
+        # Интервал между двумя пришельцами равен ширине пришельца
         alien = Alien(self)
-        self.aliens.add(alien)
+        alien_width = alien.rect.width
+        avaliable_space_x = self.settings.screen_width - (2 * alien_width)
+        number_aliens_x = avaliable_space_x // (2 * alien_width)
+
+        # Создани первого ряда пришельцев
+        for alien_number in range(number_aliens_x):
+            #
+            alien = Alien(self)
+            alien.x = alien_width + 2 * alien_width * alien_number
+            alien.rect.x = alien.x
+            self.aliens.add(alien)
 
 
 if __name__ == '__main__':
