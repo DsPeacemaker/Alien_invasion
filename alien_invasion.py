4f2f1129e5173e1bfdab8e848b9ecd6309692caf
DsPeacemaker <enotikpartizan@mail.ru>
2023-09-24 18:24:35 +0300

    Добавить уничтожение чужих

--- alien_invasion.py
+++ alien_invasion.py
@@ -82,6 +82,9 @@ class AlienInvasion:
         for bullet in self.bullets.copy():
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
+        # Проверка попаданий пришельцев.
+        # При обнаружении попадания удалить снаряд и пришельца
+        collisions = pygame.sprite.groupcollide(self.bullets, self.aliens, True, True)
 
     def _update_aliens(self):
         '''Проверяет достиг ли флот края экрана и
