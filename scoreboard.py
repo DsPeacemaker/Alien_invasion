7859f57a597878490440608061bf3bceacd87cd1
DsPeacemaker <enotikpartizan@mail.ru>
2023-09-29 15:31:49 +0300

    Изменить цвет фона, иконки, цвет текста

--- scoreboard.py
+++ scoreboard.py
@@ -29,7 +29,7 @@ class Scoreboard:
         '''Преобразует текущий счет в графическое изображение'''
         rounded_score = round(self.stats.score,  -1)
         score_str = f"{rounded_score:,}"
-        self.score_image = self.font.render(score_str, True, self.text_color, self.settings.bg_color)
+        self.score_image = self.font.render(score_str, True, self.settings.color_text, self.settings.bg_color)
 
         # Вывод счета в правой верхней части экрана
         self.score_rect = self.score_image.get_rect()
@@ -47,7 +47,7 @@ class Scoreboard:
         '''Преобразует рекордный счет в графическое изображение'''
         high_score = round(self.stats.high_score, -1)
         high_score_str = f"{high_score:,}"
-        self.high_score_image = self.font.render(high_score_str, True, self.text_color, self.settings.bg_color)
+        self.high_score_image = self.font.render(high_score_str, True, self.settings.color_text, self.settings.bg_color)
 
         # Рекорд выравнивается по центру верхней стороны
         self.high_score_rect = self.high_score_image.get_rect()
@@ -63,7 +63,7 @@ class Scoreboard:
     def prep_level(self):
         '''Преобразует уровень в графическое изображение'''
         level_str = str(self.stats.level)
-        self.level_image = self.font.render(level_str, True, self.text_color, self.settings.bg_color)
+        self.level_image = self.font.render(level_str, True, self.settings.color_text, self.settings.bg_color)
 
         # Уровень выводится под текущим счетом
         self.level_rect = self.level_image.get_rect()
--- settings.py
+++ settings.py
@@ -6,7 +6,8 @@ class Settings():
         # параметры экрана
         self.screen_width = 1200
         self.screen_height = 800
-        self.bg_color = (230, 230, 230)
+        self.bg_color = (20, 0, 0)
+        self.color_text = (230, 230, 230)
         # Настройки корабля
         self.ship_speed = 1.5
         self.ship_limit = 3
